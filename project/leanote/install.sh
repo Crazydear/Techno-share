71da23c459dbc72bb02dc1490c4ca342c657f524
李绍丰 <47840575+Crazydear@users.noreply.github.com>
2020-04-15 18:45:01 +0800

    Update install.sh

--- project/leanote/install.sh
+++ project/leanote/install.sh
@@ -58,3 +58,57 @@ bash run.sh &
 echo "恭喜你, 打开浏览器输入: http://localhost:9000 体验leanote吧!"
 cat /dev/null > ~/.bash_history && history -c && history -w
 exit 0
+
+function createCert(){
+# create self-signed server certificate:
+read -p "Enter your domain [www.example.com]: " DOMAIN
+echo "Create server key..."
+openssl genrsa -des3 -out $DOMAIN.key 1024
+echo "Create server certificate signing request..."
+SUBJECT="/C=US/ST=Mars/L=iTranswarp/O=iTranswarp/OU=iTranswarp/CN=$DOMAIN"
+openssl req -new -subj $SUBJECT -key $DOMAIN.key -out $DOMAIN.csr
+echo "Remove password..."
+mv $DOMAIN.key $DOMAIN.origin.key
+openssl rsa -in $DOMAIN.origin.key -out $DOMAIN.key
+echo "Sign SSL certificate..."
+openssl x509 -req -days 3650 -in $DOMAIN.csr -signkey $DOMAIN.key -out $DOMAIN.crt
+}
+
+function Nginx_zhuanfa(){
+[ -f /etc/nginx/conf.d/default.conf ] && mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.bak
+touch /etc/nginx/conf.d/default.conf
+cat <<EOF >/etc/nginx/conf.d/leanote.conf
+server
+    {
+        listen  80;
+        server_name  a.com;
+        
+        # 强制https
+        # 如果不需要, 请注释这一行rewrite
+        rewrite ^/(.*) https://jp_linode2.com/$1 permanent;
+        
+        location / {
+            proxy_pass        http://a.com;
+            proxy_set_header   Host             \$host;
+            proxy_set_header   X-Real-IP        \$remote_addr;
+            proxy_set_header   X-Forwarded-For  \$proxy_add_x_forwarded_for;
+        }
+    }
+    
+    # https
+    server
+    {
+        listen  443 ssl;
+        server_name  a.com;
+        ssl_certificate     /root/a.com.crt; # 修改路径, 到a.com.crt, 下同
+        ssl_certificate_key /root/a.com.key;
+        location / {
+            proxy_pass        http://a.com;
+            proxy_set_header   Host             \$host;
+            proxy_set_header   X-Real-IP        \$remote_addr;
+            proxy_set_header   X-Forwarded-For  \$proxy_add_x_forwarded_for;
+        }
+    }
+}
+EOF
+}
